52aa5e9286405e393b850a56510b12cf52bf20f3
shrinidhi99 <shrinidhi99.varna@gmail.com>
2020-03-10 17:56:47 +0530

    DAC logic

---
+++ dac.py
@@ -0,0 +1,42 @@
+# Discretionary Access Control
+
+
+class Post(object):
+    def __init__(self, post_id, user_id, content, date_posted, title):
+        self.post_id = post_id
+        self.user_id = user_id
+        self.content = content
+        self.date_posted = date_posted
+        self.title = title
+
+
+posts = [Post(1, 1, "nothing much", "12/12/12", "First"),
+         Post(2, 2, "too much", "13/12/12", "Second")]
+
+# Rows will include subjects and columns will include subjects and objects
+row_acm = []
+acm = []
+no_of_users = 4
+no_of_posts = 2
+
+for i in range(no_of_users + 1):
+    row_acm = []
+    for j in range(no_of_users + no_of_posts + 1):
+        if i == 0:
+            row_acm.append([1, 1, 1, 1])
+        else:
+            if j == i:
+                row_acm.append([1, 1, 1, 1])
+            elif j == 0 and i != 0: # no other subject has an access on admin
+                row_acm.append([0, 0, 0, 0])
+            elif (j > i) and (j >= no_of_users + 1) and (i == posts[j-no_of_users-1].user_id):
+                row_acm.append([1, 1, 1, 1])
+            elif i != j and j <= no_of_users: # a subject doesn't have an access over any other subject
+                row_acm.append([0, 0, 0, 0])
+            else:
+                print('Am here as user {}'.format(i))
+                row_acm.append([1, 0, 0, 0])
+    acm.append(row_acm)
+
+for i in range(no_of_users + 1):
+    print(acm[i])
